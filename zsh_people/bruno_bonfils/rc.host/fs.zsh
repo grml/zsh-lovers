5cb88f9fb34ecec3c6fdf321e4f5842da0102ef1
Michael Prokop <mika@grml.org>
2014-04-03 10:32:01 +0200

    Get rid of files where we don't own the copyright or license is unclear
    Thanks: Ulrich Mueller <ulm@gentoo.org> for raising the issue

--- zsh_people/bruno_bonfils/rc.host/fs.zsh
+++
@@ -1,6 +0,0 @@
-# Prompts color
-
-export host_color="yellow"
-export domain_color="yellow"
-export path_color="yellow"
-export date_color="white"
--- zsh_people/bruno_bonfils/rc.host/julie.zsh
+++
@@ -1,6 +0,0 @@
-# Prompts color
-
-export host_color="green"
-export domain_color="green"
-export path_color="green"
-export date_color="white"
